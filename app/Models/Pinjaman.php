5f64b809abe7865be1b1ba93e85f7af40db2ef37
Ridamasrifaa <sukavanilaa25@gmail.com>
2026-02-04 10:04:35 +0700

    feat(pinjaman.php):menambahkan code

--- app/Models/Pinjaman.php
+++ app/Models/Pinjaman.php
@@ -6,5 +6,33 @@ use Illuminate\Database\Eloquent\Model;
 
 class Pinjaman extends Model
 {
-    //
+    protected $table = 'pinjaman';
+
+    protected $fillable = [
+        'anggota_id',
+        'approved_by',
+        'jumlah_pinjaman',
+        'bunga_persen',
+        'tenor',
+        'status',
+        'tanggal_pinjaman',
+        'total_pinjaman'
+    ];
+
+    public function anggota()
+    {
+        return $this->belongsTo(Anggota::class, 'anggota_id');
+    }
+    protected static function booted()
+{
+    static::creating(function ($pinjaman) {
+        $bunga = 2; // tetap 2%
+        $pinjaman->total_pinjaman = $pinjaman->jumlah_pinjaman + ($pinjaman->jumlah_pinjaman * $bunga / 100);
+
+        if (!$pinjaman->status) {
+            $pinjaman->status = 'pending';
+        }
+    });
+}
+
 }
