b1bff14b824ae7f7a8392d67caadb01ffa8848be
zLDevv <242510199@smkn4-tsm.sch.id>
2026-02-05 11:23:46 +0700

    feat(angsuran.php):menambahkan file baru dan code

--- app/Models/Angsuran.php
+++ app/Models/Angsuran.php
@@ -2,9 +2,25 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Angsuran extends Model
 {
-    //
+    use HasFactory;
+
+    protected $table = 'angsurans';
+
+    protected $fillable = [
+        'pinjaman_id',
+        'angsuran_ke',
+        'jumlah',
+        'tanggal_bayar'
+    ];
+
+    // relasi ke pinjaman
+    public function pinjaman()
+    {
+        return $this->belongsTo(Pinjaman::class);
+    }
 }
