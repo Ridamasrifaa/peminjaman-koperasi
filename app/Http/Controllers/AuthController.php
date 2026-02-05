4a574c886cc82a37c90202a931743f5263d91f13
zLDevv <242510199@smkn4-tsm.sch.id>
2026-02-05 11:22:05 +0700

    feat(AuthController.php):menambahkan code

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        if (Auth::attempt($request->only('email', 'password'))) {
+            $request->session()->regenerate();
+            return redirect('/anggota');
+        }
+
+        return back()->withErrors([
+            'email' => 'Email atau password salah'
+        ]);
+    }
+}
