27316f83c580e86ae52fc547cf22e8649387da5e
zLDevv <242510199@smkn4-tsm.sch.id>
2026-02-05 11:20:44 +0700

    feat(simpanan.php):menambahkan perubahan

--- app/Models/Simpanan.php
+++ app/Models/Simpanan.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Simpanan extends Model
 {
-    //
+    protected $table = 'simpanan'; // NAMA TABEL ASLI
+    protected $primaryKey = 'id';
+
+    protected $fillable = [
+        'anggota_id',
+        'jumlah'
+    ];
+
+    public $timestamps = true;
 }
