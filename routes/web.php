8e19db32ae6cfcf5ce3e4ce4cec7bca8a695afd1
zLDevv <242510199@smkn4-tsm.sch.id>
2026-02-05 11:25:45 +0700

    feat(web.php):menambahkan code

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,22 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AnggotaController;
+use App\Http\Controllers\AuthController;
 
+Route::get('/', function () {
+    return view('welcome');
+});
 
-Route::get('/', function () {return view('welcome');});
+Route::get('/login', function () {
+    return view('login');
+});
+
+Route::post('/login', [AuthController::class, 'login'])->name('login');
+
 Route::get('/anggota', [AnggotaController::class, 'index']);
+
+// TEST SESSION
+Route::get('/test-session', function () {
+    session(['cek' => 'jalan']);
+    return session('cek');
+});
