a353983ad9834cca40892744732c64513a8654ab
Ridamasrifaa <sukavanilaa25@gmail.com>
2026-02-06 14:45:15 +0700

    remove filament and update dependencies

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -7,20 +7,28 @@ use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
-    public function login(Request $request)
-    {
-        $request->validate([
-            'email' => 'required|email',
-            'password' => 'required'
-        ]);
+public function login(Request $request)
+{
+    $request->validate([
+        'email' => 'required|email',
+        'password' => 'required'
+    ]);
 
-        if (Auth::attempt($request->only('email', 'password'))) {
-            $request->session()->regenerate();
-            return redirect('/anggota');
+    if (Auth::attempt($request->only('email', 'password'))) {
+        $request->session()->regenerate();
+
+        $user = Auth::user(); // ambil data user yang login
+
+        if ($user->role === 'admin' || $user->role === 'sekertaris') {
+            return redirect('/admin/dashboard'); // admin & sekretaris ke dashboard admin
+        } elseif ($user->role === 'anggota') {
+            return redirect('/anggota'); // anggota ke halaman anggota
         }
-
-        return back()->withErrors([
-            'email' => 'Email atau password salah'
-        ]);
     }
+
+    return back()->withErrors([
+        'email' => 'Email atau password salah'
+    ]);
+}
+
 }
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AnggotaController;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\AdminDashboardController;
+
 
 Route::get('/', function () {
     return view('welcome');
@@ -16,8 +18,14 @@ Route::post('/login', [AuthController::class, 'login'])->name('login');
 
 Route::get('/anggota', [AnggotaController::class, 'index']);
 
-// TEST SESSION
+Route::get('/admin/dashboard', [AdminDashboardController::class, 'index'])
+    ->middleware('auth', 'role:admin,sekertaris');
+
+
 Route::get('/test-session', function () {
     session(['cek' => 'jalan']);
     return session('cek');
 });
+
+
+
